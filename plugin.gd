1db0e29e1bcfb6f23819b45734f3f51e1dee9a77
legole0 <legole0@proton.me>
2026-04-10 17:47:35 -0300

    Use new EditorDock class, as opposed to deprecated functions

--- plugin.gd
+++ plugin.gd
@@ -2,19 +2,27 @@
 extends EditorPlugin
 
 static var importer_scene:Control
+var dock:EditorDock
 
 func _enter_tree() -> void:
 	ImportUtils.addon_path = get_script().resource_path.get_base_dir()+"/"
 	importer_scene = load(ImportUtils.addon_path+"PluginScene.tscn").instantiate()
-	#add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_UR, importer_scene)
-	#EditorInterface.get_editor_main_screen().add_child(importer_scene)
-	add_control_to_bottom_panel(importer_scene, _get_plugin_name())
 	
-	#print("Sprite Importer initialized successfully!")
+	dock = EditorDock.new()
+	dock.title = _get_plugin_name()
+	dock.icon_name = &"SpriteFrames"
+	dock.default_slot = EditorDock.DOCK_SLOT_BOTTOM
+	dock.available_layouts = EditorDock.DOCK_LAYOUT_HORIZONTAL | EditorDock.DOCK_LAYOUT_HORIZONTAL
+	dock.connect(&"closed", _close_dock)
+	dock.add_child(importer_scene)
+	add_dock(dock)
 
 func _exit_tree() -> void:
-	remove_control_from_bottom_panel(importer_scene)
+	remove_dock(dock)
 	importer_scene.queue_free()
 
 func _get_plugin_name():
 	return "Sprite Importer"
+
+func _close_dock() -> void:
+	EditorInterface.set_plugin_enabled("sprite_importer", false)
